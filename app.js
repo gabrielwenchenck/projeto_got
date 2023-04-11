6de76c23b9b758fdaa6d9e94b0a4d7da7af32daa
Gabriel Wenchenck <104534121+gabrielwenchenck@users.noreply.github.com>
2023-04-11 16:23:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 /* importar as configurações do servidor */
 var app = require("./config/server");
+var dotenv = require("dotenv");
+
+dotenv.config();
 
 /* parametrizar a porta de escuta */
-app.listen(8080, function () {
+app.listen(process.env.PORT, function () {
   console.log("Servidor online");
 });
